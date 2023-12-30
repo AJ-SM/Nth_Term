b3b309f9f7fbd1e1294fc59fe3c51568ae421097
AJ-SM <154649323+AJ-SM@users.noreply.github.com>
2023-12-30 14:10:52 +0530

    Create mian.py

---
+++ mian.py
@@ -0,0 +1,16 @@
+
+
+
+
+
+
+print ("-------Welcome_To_Nth_Programme_--------")
+print ("if any value is not given then put x there....")
+a = int(input("Enter  A :"))
+n = int(input("Enter  N :"))
+d = int(input("Enter  D :"))
+m = n - 1
+k = m*d
+h = k + a
+
+print (f"Your Answer is {h}")
